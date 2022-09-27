4559d2ba004d9f21b8b0e4242f4766a5c998e04e
nasimuddin01 <debater.nasim@gmail.com>
2022-09-28 04:09:50 +0600

    Cors allowed origin add

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,21 @@ const cors = require("cors");
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:8080", "https://cionic.onrender.com"];
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      console.log(origin);
+      if (!allowedOrigins.includes(origin)) {
+        const msg = `The CORS policy for ${origin} does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`App running on port ${port}!`));
